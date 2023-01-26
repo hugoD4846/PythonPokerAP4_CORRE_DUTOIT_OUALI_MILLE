033cf4563937d128c968e1de6b521e4384521d86
leooz <leo59coc@gmail.com>
2023-01-26 15:56:38 +0100

    cool

--- exemple.py
+++ exemple.py
@@ -1,10 +1,13 @@
 from card_deck import Card, Deck, Color, Value
 from combinaison import Combinaison
 from finder import find_combinaisons
+from logConf import setUpLogging
+import logging
 
 if __name__ == "__main__":
-    deck = Deck() # create a deck
-    hand = [deck.pop() for _ in range(7)] # get 7 cards from the deck to create an hand and remove them from the deck  with pop() method
-    combinaison = find_combinaisons(hand) # find combinaisons in the hand
-    print(combinaison) # print the result
-
+    setUpLogging(logging.INFO)
+    deck = Deck()  # create a deck
+    # get 7 cards from the deck to create an hand and remove them from the deck  with pop() method
+    hand = [deck.pop() for _ in range(7)]
+    combinaison = find_combinaisons(hand)  # find combinaisons in the hand
+    print(combinaison)  # print the result
